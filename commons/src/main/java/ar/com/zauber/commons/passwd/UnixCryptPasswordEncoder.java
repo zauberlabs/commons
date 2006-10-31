c037ea2bb43c5883c8e66470062b6f9232018a0b
Juan F. Codagnone <juan@zaubersoftware.com>
2006-10-31 04:17:48 +0000

    codificado de password con la funcion crypt de unix

---
+++ commons/src/main/java/ar/com/zauber/commons/passwd/UnixCryptPasswordEncoder.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2006 Zauber  -- All rights reserved
+ */
+package ar.com.zauber.commons.passwd;
+
+
+/**
+ * Codifica password con la funcion unix Crypt()
+ * 
+ * @author Juan F. Codagnone
+ * @since Oct 31, 2006
+ */
+public class UnixCryptPasswordEncoder implements PasswordEncoder {
+
+    /**  @see PasswordEncoder#encodePassword(String) */
+    public final String encodePassword(final String password) {
+        return UnixCrypt.crypt(password);
+    }
+}
