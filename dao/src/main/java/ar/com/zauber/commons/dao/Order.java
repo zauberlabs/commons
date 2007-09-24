eaeaed62d43c50d62a744e701d37e83f749a0c0d
Martín Marquez <martin@zaubersoftware.com>
2007-09-24 14:11:12 +0000

    Agregado de clase para representar ordenamiento.

---
+++ dao/src/main/java/ar/com/zauber/commons/dao/Order.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2007 Garbarino S.A.  -- Todos los derechos reservados
+ */
+
+package ar.com.zauber.commons.dao;
+        
+
+/**
+ * TODO Descripcion de la clase. Los comenterios van en castellano.
+ * 
+ * 
+ * @author Martin A. Marquez
+ * @since Sep 24, 2007
+ */
+public class Order {
+
+}
