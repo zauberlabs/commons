e84a95172e29dfbce2f9ba9002919c3a929bdd5e
Martín Marquez <martin@zaubersoftware.com>
2007-09-24 14:11:56 +0000

    Parece que asi esta, tiene una lista de Strings (campos de ordenamiento) y un booleano que indica si es o no ascendente.

--- dao/src/main/java/ar/com/zauber/commons/dao/Order.java
+++ dao/src/main/java/ar/com/zauber/commons/dao/Order.java
@@ -3,10 +3,12 @@
  */
 
 package ar.com.zauber.commons.dao;
+
+import java.util.List;
         
 
 /**
- * TODO Descripcion de la clase. Los comenterios van en castellano.
+ * Representa un orden.
  * 
  * 
  * @author Martin A. Marquez
@@ -14,4 +16,15 @@ package ar.com.zauber.commons.dao;
  */
 public class Order {
 
+    
+    /** <code>properties</code> */
+    private List<String> properties;
+    
+    /** <code>direction</code> */
+    private Boolean ascending;
+    
+    public static final Boolean ASCENDING = Boolean.TRUE;
+    public static final Boolean DESCENDING = Boolean.FALSE;
+    
+    
 }
