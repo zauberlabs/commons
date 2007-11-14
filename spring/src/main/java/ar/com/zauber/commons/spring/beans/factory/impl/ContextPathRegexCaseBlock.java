c0ee4d4d34687b8874900aff7e86d3cd68d94cba
Juan F. Codagnone <juan@zaubersoftware.com>
2007-11-14 02:42:22 +0000

    ImplementaciÃ³n de CaseBlock? que permite preguntar por donde estÃ¡ desplegada la aplicaciÃ³n.
    Ej:
    <bean name="test1" .zauber.commons.spring.beans.factory.SwitchConditionalFactoryBean"> 
       <property name="caseBlocks">
             <list>
                  <bean .zauber.commons.spring.beans.factory.impl.ContextPathRegexCaseBlock"
                        <constructor-arg index="0"  value="^.*home$"/>
                        <constructor-arg index="1" value="home"/>
                  </bean>
                  <bean .zauber.commons.spring.beans.factory.impl.ContextPathRegexCaseBlock"
                        <constructor-arg index="0"
    se$"/>
                        <constructor-arg index="1" value="house"/>
                  </bean
           .............

---
+++ spring/src/main/java/ar/com/zauber/commons/spring/beans/factory/impl/ContextPathRegexCaseBlock.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2007 Zauber S.A.  -- All rights reserved
+ */
+package ar.com.zauber.commons.spring.beans.factory.impl;
+
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.Validate;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.web.context.WebApplicationContext;
+
+import ar.com.zauber.commons.spring.beans.factory.CaseBlock;
+
+
+/**
+ * Implementación de {@link CaseBlock} que permite preguntar por donde 
+ * está desplegada la apliación. Ej:
+ * 
+ * <pre>
+ * <bean name="test1" class="ar.com.zauber.commons.spring.beans.factory.SwitchConditionalFactoryBean">
+ *     <property name="caseBlocks">
+ *         <list>
+ *            <bean class="ar.com.zauber.commons.spring.beans.factory.impl.ContextPathRegexCaseBlock" >
+ *               <constructor-arg index="0"  value="^.*home$"/>
+ *               <constructor-arg index="1" value="home"/>
+ *            </bean>
+ *            <bean class="ar.com.zauber.commons.spring.beans.factory.impl.ContextPathRegexCaseBlock" >
+ *               <constructor-arg index="0"  value="^.*house$"/>
+ *               <constructor-arg index="1" value="house"/>
+ *            </bean>
+ *      </property>
+ *  </bean>
+ * </pre>
+ * 
+ * @author Juan F. Codagnone
+ * @since Nov 13, 2007
+ */
+public class ContextPathRegexCaseBlock implements CaseBlock, 
+                                                  ApplicationContextAware {
+    private final Pattern regex;
+    private final Object object;
+    private WebApplicationContext ctx;
+    
+    /**
+     * Creates the ContextPathRegexCaseBlock.
+     *
+     */
+    public ContextPathRegexCaseBlock(final String regex,
+            final Object object) {
+        Validate.notEmpty(regex);
+        
+        this.regex = Pattern.compile(regex);
+        this.object = object;
+    }
+    
+    /** @see CaseBlock#evaluate() */
+    public final boolean evaluate() {
+        return regex.matcher(ctx.getServletContext().getRealPath("/")).lookingAt();
+    }
+
+    /** @see CaseBlock#getObject() */
+    public final Object getObject() {
+        return object;
+    }
+
+    /** @see ApplicationContextAware#setApplicationContext(ApplicationContext) */
+    public final void setApplicationContext(final ApplicationContext ctx)
+            throws BeansException {
+        this.ctx = (WebApplicationContext)ctx;
+    }
+
+}
---
+++ spring/src/test/java/ar/com/zauber/commons/spring/beans/factory/impl/ContextPathRegexCaseBlockTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2007 Zauber S.A.  -- All rights reserved
+ */
+package ar.com.zauber.commons.spring.beans.factory.impl;
+
+import javax.servlet.ServletContextEvent;
+
+import junit.framework.TestCase;
+
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.web.context.WebApplicationContext;
+
+import ar.com.zauber.commons.spring.mock.TestFriendlyContextLoader;
+import ar.com.zauber.commons.spring.mock.TestFriendlyContextLoaderListener;
+
+/**
+ * TestT
+ * 
+ * 
+ * @author Juan F. Codagnone
+ * @since Nov 13, 2007
+ */
+public class ContextPathRegexCaseBlockTest extends TestCase {
+
+    /** test */
+    public final void testCtxHome() {
+        final MockServletContext servletCtx = new MockServletContext("/home");
+        servletCtx.addInitParameter("contextConfigLocation", 
+            "classpath:/spring-test-switch-regex.xml");
+        final TestFriendlyContextLoaderListener listener = 
+            new TestFriendlyContextLoaderListener();
+        listener.contextInitialized(new ServletContextEvent(servletCtx));
+        
+        final WebApplicationContext ctx = 
+            ((TestFriendlyContextLoader)listener.getContextLoader()).getCtx();
+        
+        assertEquals("home", ctx.getBean("test1"));
+    }
+    
+    /** test */
+    public final void testCtxHouse() {
+        final MockServletContext servletCtx = new MockServletContext("/house");
+        servletCtx.addInitParameter("contextConfigLocation", 
+            "classpath:/spring-test-switch-regex.xml");
+        final TestFriendlyContextLoaderListener listener = 
+            new TestFriendlyContextLoaderListener();
+        listener.contextInitialized(new ServletContextEvent(servletCtx));
+        
+        final WebApplicationContext ctx = 
+            ((TestFriendlyContextLoader)listener.getContextLoader()).getCtx();
+        
+        assertEquals("house", ctx.getBean("test1"));
+    }
+    
+    
+    /** test */
+    public final void testCtxDefault() {
+        final MockServletContext servletCtx = new MockServletContext("/laralara");
+        servletCtx.addInitParameter("contextConfigLocation", 
+            "classpath:/spring-test-switch-regex.xml");
+        final TestFriendlyContextLoaderListener listener = 
+            new TestFriendlyContextLoaderListener();
+        listener.contextInitialized(new ServletContextEvent(servletCtx));
+        
+        final WebApplicationContext ctx = 
+            ((TestFriendlyContextLoader)listener.getContextLoader()).getCtx();
+        
+        assertEquals("default", ctx.getBean("test1"));
+    }
+}
