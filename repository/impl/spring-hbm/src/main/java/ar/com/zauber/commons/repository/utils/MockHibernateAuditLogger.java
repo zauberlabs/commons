6a679e019bb53be5e6badc27d04cdc5102189f99
Juan F. Codagnone <juan@zaubersoftware.com>
2008-03-19 14:09:25 +0000

    checkstyle

--- repository/impl/spring-hbm/src/main/java/ar/com/zauber/commons/repository/utils/MockHibernateAuditLogger.java
+++ repository/impl/spring-hbm/src/main/java/ar/com/zauber/commons/repository/utils/MockHibernateAuditLogger.java
@@ -16,131 +16,131 @@ import org.hibernate.type.Type;
 
 /**
  * Mock implementation of the logger.
- * 
- * 
+ *
+ *
  * @author Martin A. Marquez
  * @since Jan 30, 2008
  */
 public class MockHibernateAuditLogger implements Interceptor {
 
-    /** @see org.hibernate.Interceptor#afterTransactionBegin(org.hibernate.Transaction) */
-    public void afterTransactionBegin(Transaction tx) {
-        // TODO Auto-generated method stub
-        
+    //CHECKSTYLE:DESIGN:OFF
+    /** @see Interceptor#afterTransactionBegin(Transaction) */
+    public void afterTransactionBegin(final Transaction tx) {
+        // void
     }
 
-    /** @see org.hibernate.Interceptor#afterTransactionCompletion(org.hibernate.Transaction) */
-    public void afterTransactionCompletion(Transaction tx) {
-        // TODO Auto-generated method stub
-        
+    /** @see Interceptor#afterTransactionCompletion(Transaction) */
+    public void afterTransactionCompletion(final Transaction tx) {
+        // void
     }
 
-    /** @see org.hibernate.Interceptor#beforeTransactionCompletion(org.hibernate.Transaction) */
-    public void beforeTransactionCompletion(Transaction tx) {
-        // TODO Auto-generated method stub
-        
+    /** @see Interceptor#beforeTransactionCompletion(Transaction) */
+    public void beforeTransactionCompletion(final Transaction tx) {
+        // void
     }
 
-    /** @see org.hibernate.Interceptor#findDirty(java.lang.Object, java.io.Serializable, java.lang.Object[], java.lang.Object[], java.lang.String[], org.hibernate.type.Type[]) */
-    public int[] findDirty(Object entity, Serializable id,
-            Object[] currentState, Object[] previousState,
-            String[] propertyNames, Type[] types) {
-        // TODO Auto-generated method stub
+    /** @see Interceptor#findDirty(Object, Serializable, Object[], Object[], 
+     *                             String[], Type[]) */
+    public int[] findDirty(final Object entity, final Serializable id,
+            final Object[] currentState, final Object[] previousState,
+            final String[] propertyNames, final Type[] types) {
+        // nada que hacer
         return null;
     }
 
-    /** @see org.hibernate.Interceptor#getEntity(java.lang.String, java.io.Serializable) */
-    public Object getEntity(String entityName, Serializable id)
+    /** @see Interceptor#getEntity(String, Serializable) */
+    public Object getEntity(final String entityName, final Serializable id)
             throws CallbackException {
-        // TODO Auto-generated method stub
+        // nada que hacer
         return null;
     }
 
     /** @see org.hibernate.Interceptor#getEntityName(java.lang.Object) */
-    public String getEntityName(Object object) throws CallbackException {
-        // TODO Auto-generated method stub
+    public String getEntityName(final Object object) throws CallbackException {
+        // nada que hacer
         return null;
     }
 
-    /** @see org.hibernate.Interceptor#instantiate(java.lang.String, org.hibernate.EntityMode, java.io.Serializable) */
-    public Object instantiate(String entityName, EntityMode entityMode,
-            Serializable id) throws CallbackException {
-        // TODO Auto-generated method stub
+    /** @see Interceptor#instantiate(String, EntityMode, Serializable) */
+    public Object instantiate(final String entityName, final EntityMode entityMode,
+            final Serializable id) throws CallbackException {
+        // nada que hacer
         return null;
     }
 
-    /** @see org.hibernate.Interceptor#isTransient(java.lang.Object) */
-    public Boolean isTransient(Object entity) {
-        // TODO Auto-generated method stub
+    /** @see Interceptor#isTransient(Object) */
+    public Boolean isTransient(final Object entity) {
+        // nada que hacer
         return null;
     }
 
-    /** @see org.hibernate.Interceptor#onCollectionRecreate(java.lang.Object, java.io.Serializable) */
-    public void onCollectionRecreate(Object collection, Serializable key)
+    /** @see Interceptor#onCollectionRecreate(Object, Serializable) */
+    public void onCollectionRecreate(final Object collection, final Serializable key)
             throws CallbackException {
-        // TODO Auto-generated method stub
-        
+        // nada que hacer
     }
 
-    /** @see org.hibernate.Interceptor#onCollectionRemove(java.lang.Object, java.io.Serializable) */
-    public void onCollectionRemove(Object collection, Serializable key)
+    /** @see Interceptor#onCollectionRemove(java.lang.Object, java.io.Serializable) */
+    public void onCollectionRemove(final Object collection, final Serializable key)
             throws CallbackException {
-        // TODO Auto-generated method stub
-        
+        // nada que hacer
     }
 
-    /** @see org.hibernate.Interceptor#onCollectionUpdate(java.lang.Object, java.io.Serializable) */
-    public void onCollectionUpdate(Object collection, Serializable key)
+    /** @see Interceptor#onCollectionUpdate(Object, Serializable) */
+    public void onCollectionUpdate(final Object collection, final Serializable key)
             throws CallbackException {
-        // TODO Auto-generated method stub
-        
+        // nada que hacer
     }
 
-    /** @see org.hibernate.Interceptor#onDelete(java.lang.Object, java.io.Serializable, java.lang.Object[], java.lang.String[], org.hibernate.type.Type[]) */
-    public void onDelete(Object entity, Serializable id, Object[] state,
-            String[] propertyNames, Type[] types) throws CallbackException {
+    /** @see Interceptor#onDelete(Object, Serializable, Object[], String[], 
+     *                            Type[]) */
+    public void onDelete(final Object entity, final Serializable id, 
+            final Object[] state, final String[] propertyNames, 
+            final Type[] types) throws CallbackException {
         // TODO Auto-generated method stub
-        
+
     }
 
-    /** @see org.hibernate.Interceptor#onFlushDirty(java.lang.Object, java.io.Serializable, java.lang.Object[], java.lang.Object[], java.lang.String[], org.hibernate.type.Type[]) */
-    public boolean onFlushDirty(Object entity, Serializable id,
-            Object[] currentState, Object[] previousState,
-            String[] propertyNames, Type[] types) throws CallbackException {
+    /** @see Interceptor#onFlushDirty(Object, Serializable, Object[], Object[], 
+     *                                String[], Type[]) */
+    public boolean onFlushDirty(final Object entity, final Serializable id,
+            final Object[] currentState, final Object[] previousState,
+            final String[] propertyNames, final Type[] types) 
+            throws CallbackException {
         // TODO Auto-generated method stub
         return false;
     }
 
-    /** @see org.hibernate.Interceptor#onLoad(java.lang.Object, java.io.Serializable, java.lang.Object[], java.lang.String[], org.hibernate.type.Type[]) */
-    public boolean onLoad(Object entity, Serializable id, Object[] state,
-            String[] propertyNames, Type[] types) throws CallbackException {
+    /** @see org.hibernate.Interceptor#onLoad(Object, Serializable, Object[], 
+     *                                  String[], org.hibernate.type.Type[]) */
+    public boolean onLoad(final Object entity, final Serializable id, 
+            final Object[] state, final String[] propertyNames, 
+            final Type[] types) throws CallbackException {
         // TODO Auto-generated method stub
         return false;
     }
 
     /** @see org.hibernate.Interceptor#onPrepareStatement(java.lang.String) */
-    public String onPrepareStatement(String sql) {
+    public String onPrepareStatement(final String sql) {
         // TODO Auto-generated method stub
         return null;
     }
 
-    /** @see org.hibernate.Interceptor#onSave(java.lang.Object, java.io.Serializable, java.lang.Object[], java.lang.String[], org.hibernate.type.Type[]) */
-    public boolean onSave(Object entity, Serializable id, Object[] state,
-            String[] propertyNames, Type[] types) throws CallbackException {
-        // TODO Auto-generated method stub
+    /** @see Interceptor#onSave(Object, Serializable, Object[], String[], Type[]) */
+    public final boolean onSave(final Object entity, final Serializable id, 
+            final Object[] state, final String[] propertyNames, 
+            final Type[] types) throws CallbackException {
         return false;
     }
 
     /** @see org.hibernate.Interceptor#postFlush(java.util.Iterator) */
-    public void postFlush(Iterator entities) throws CallbackException {
-        // TODO Auto-generated method stub
-        
+    public void postFlush(final Iterator entities) throws CallbackException {
+        // nada que hacer
+
     }
 
     /** @see org.hibernate.Interceptor#preFlush(java.util.Iterator) */
-    public void preFlush(Iterator entities) throws CallbackException {
-        // TODO Auto-generated method stub
-        
+    public void preFlush(final Iterator entities) throws CallbackException {
+        // nada que hacer
     }
-
 }
