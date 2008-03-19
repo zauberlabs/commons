6f64826117e4083da961783720ec473a256fdcff
Juan F. Codagnone <juan@zaubersoftware.com>
2008-03-19 14:52:41 +0000

    HibernateAuditLoggerInterceptor: un poco de estilo y convenciones. y además el IoC se hacia por setter y no por constructor. acostumbramos a injectar dependencias para validar facilmente que no se olvidó de setear ninguna. Para no cambiar la interface de servicio hago que la clase implemente InitializingBean para poder validar que no falte setear dependencias (que despues en ejecucion mas tarde serán NPE).
    https://wiki.zauber.com.ar/intra/bin/view/Metodologia/WebHome#Buenas_Pr_cticas
    http://docs.codehaus.org/display/PICO/Good+Citizen

--- repository/impl/spring-hbm/src/main/java/ar/com/zauber/commons/repository/utils/HibernateAuditLoggerInterceptor.java
+++ repository/impl/spring-hbm/src/main/java/ar/com/zauber/commons/repository/utils/HibernateAuditLoggerInterceptor.java
@@ -12,162 +12,188 @@ import org.hibernate.EntityMode;
 import org.hibernate.Interceptor;
 import org.hibernate.Transaction;
 import org.hibernate.type.Type;
+import org.springframework.beans.factory.InitializingBean;
 
 import ar.com.zauber.commons.auth.AuthenticationUserMapper;
 import ar.com.zauber.commons.repository.CreationAuditable;
 import ar.com.zauber.commons.repository.ModificationAuditable;
 
-
-public class HibernateAuditLoggerInterceptor implements Interceptor {
+//CHECKSTYLE:DESIGN:OFF
+/**
+ * Interceptor hibernate
+ * 
+ * 
+ * @author Martin Marquez
+ */
+public class HibernateAuditLoggerInterceptor implements Interceptor, 
+                                                        InitializingBean {
 
     private AuthenticationUserMapper<String> authenticationUserMapper;
-    
-    public void setAuthenticationUserMapper(AuthenticationUserMapper<String> authenticationUserMapper) {
+
+    public void setAuthenticationUserMapper(
+            final AuthenticationUserMapper<String> authenticationUserMapper) {
         this.authenticationUserMapper = authenticationUserMapper;
     }
 
-
+    /** @see InitializingBean#afterPropertiesSet() */
+    public final void afterPropertiesSet() throws Exception {
+        if(authenticationUserMapper == null) {
+            throw new IllegalArgumentException(
+                    "falta setear el authentication user mapper");
+        }
+    }
+    
     /** @see Interceptor#onLoad(Object, Serializable, Object[], String[], Type[]) */
-    public boolean onLoad(Object arg0, Serializable arg1, Object[] arg2, String[] arg3, Type[] arg4)
+    public boolean onLoad(final Object arg0, final Serializable arg1,
+            final Object[] arg2, final String[] arg3, final Type[] arg4)
             throws CallbackException {
         // TODO Auto-generated method stub
         return false;
     }
 
+    /**
+     * @see Interceptor#onFlushDirty(Object, Serializable, Object[], Object[],
+     *      String[], Type[])
+     */
+    public boolean onFlushDirty(final Object obj, final Serializable id,
+            final Object[] newValues, final Object[] oldValues,
+            final String[] properties, final Type[] types)
+            throws CallbackException {
 
-    /** @see Interceptor#onFlushDirty(Object, Serializable, Object[], Object[], String[], Type[]) */
-    public boolean onFlushDirty(Object obj, Serializable id, Object[] newValues, Object[] oldValues,
-            String[] properties, Type[] types) throws CallbackException {
-
-        if (obj instanceof ModificationAuditable) 
-        {
-            for(int i = 0; i < properties.length; i++) {
-                if(properties[i].equals("modifiedAt")) {
+        if (obj instanceof ModificationAuditable) {
+            for (int i = 0; i < properties.length; i++) {
+                if (properties[i].equals("modifiedAt")) {
                     newValues[i] = new Date();
                 }
-                if(properties[i].equals("modifiedBy")) {
+                if (properties[i].equals("modifiedBy")) {
                     newValues[i] = authenticationUserMapper.getUser();
                 }
             }
         }
-        
+
         return true;
     }
 
-
     /** @see Interceptor#onSave(Object, Serializable, Object[], String[], Type[]) */
-    public boolean onSave(Object obj, Serializable id, Object[] newValues, String[] properties, Type[] types)
-            throws CallbackException {
+    public boolean onSave(final Object obj, final Serializable id,
+            final Object[] newValues, final String[] properties,
+            final Type[] types) throws CallbackException {
 
-        
-        if (obj instanceof CreationAuditable) 
-        {
-            for(int i = 0; i < properties.length; i++) {
-                if(properties[i].equals("createdAt")) {
+        if (obj instanceof CreationAuditable) {
+            for (int i = 0; i < properties.length; i++) {
+                if (properties[i].equals("createdAt")) {
                     newValues[i] = new Date();
                 }
-                if(properties[i].equals("createdBy")) {
+                if (properties[i].equals("createdBy")) {
                     newValues[i] = authenticationUserMapper.getUser();
                 }
                 if (obj instanceof ModificationAuditable) {
-                    if(properties[i].equals("modifiedAt")) {
+                    if (properties[i].equals("modifiedAt")) {
                         newValues[i] = new Date();
                     }
-                    if(properties[i].equals("modifiedBy")) {
+                    if (properties[i].equals("modifiedBy")) {
                         newValues[i] = authenticationUserMapper.getUser();
                     }
                 }
             }
         }
-        
+
         return true;
     }
 
-    
-    /** @see Interceptor#onDelete(Object, Serializable, Object[], String[], Type[]) */
-    public void onDelete(Object obj, Serializable id, Object[] newValues, String[] properties, Type[] types)
-            throws CallbackException {
+    /**
+     * @see Interceptor#onDelete(Object, Serializable, Object[], String[],
+     *      Type[])
+     */
+    public void onDelete(final Object obj, final Serializable id,
+            final Object[] newValues, final String[] properties,
+            final Type[] types) throws CallbackException {
     }
 
     /** @see Interceptor#preFlush(Iterator) */
-    public void preFlush(Iterator arg0) throws CallbackException {
+    public void preFlush(final Iterator arg0) throws CallbackException {
         // TODO Auto-generated method stub
     }
-
 
     /** @see Interceptor#postFlush(Iterator) */
-    public void postFlush(Iterator arg0) throws CallbackException {
-        // TODO Auto-generated method stub
+    public void postFlush(final Iterator arg0) throws CallbackException {
+        // nada que hacer
     }
 
-    /** @see Interceptor#findDirty(Object, Serializable, Object[], Object[], String[], Type[]) */
-    public int[] findDirty(Object arg0, Serializable arg1, Object[] arg2, Object[] arg3, String[] arg4, Type[] arg5) {
+    /**
+     * @see Interceptor#findDirty(Object, Serializable, Object[], Object[],
+     *      String[], Type[])
+     */
+    public int[] findDirty(final Object arg0, final Serializable arg1,
+            final Object[] arg2, final Object[] arg3, final String[] arg4,
+            final Type[] arg5) {
         // TODO Auto-generated method stub
         return null;
     }
 
     /** @see Interceptor#isTransient(Object) */
-    public Boolean isTransient(Object arg0) {
+    public Boolean isTransient(final Object arg0) {
         // TODO Auto-generated method stub
         return null;
     }
 
     /** @see Interceptor#instantiate(String, EntityMode, Serializable) */
-    public Object instantiate(String arg0, EntityMode arg1, Serializable arg2) throws CallbackException {
+    public Object instantiate(final String arg0, final EntityMode arg1,
+            final Serializable arg2) throws CallbackException {
         // TODO Auto-generated method stub
         return null;
     }
 
     /** @see Interceptor#getEntityName(Object) */
-    public String getEntityName(Object arg0) throws CallbackException {
-        // TODO Auto-generated method stub
+    public String getEntityName(final Object arg0) throws CallbackException {
+        // nada que hacer
         return null;
     }
 
     /** @see Interceptor#getEntity(String, Serializable) */
-    public Object getEntity(String arg0, Serializable arg1) throws CallbackException {
-        // TODO Auto-generated method stub
+    public Object getEntity(final String arg0, final Serializable arg1)
+            throws CallbackException {
+        // nada que hacer
         return null;
     }
 
     /** @see Interceptor#afterTransactionBegin(Transaction) */
-    public void afterTransactionBegin(Transaction arg0) {
+    public void afterTransactionBegin(final Transaction arg0) {
         // TODO Auto-generated method stub
     }
 
     /** @see Interceptor#beforeTransactionCompletion(Transaction) */
-    public void beforeTransactionCompletion(Transaction arg0) {
+    public void beforeTransactionCompletion(final Transaction arg0) {
         // TODO Auto-generated method stub
 
     }
 
     /** @see Interceptor#afterTransactionCompletion(Transaction) */
-    public void afterTransactionCompletion(Transaction arg0) {
+    public void afterTransactionCompletion(final Transaction arg0) {
         // TODO Auto-generated method stub
     }
 
     /** @see Interceptor#onCollectionRecreate(Object, Serializable) */
-    public void onCollectionRecreate(Object collection, Serializable key) throws CallbackException {
+    public void onCollectionRecreate(final Object collection,
+            final Serializable key) throws CallbackException {
         // TODO Auto-generated method stub
-        
+
     }
 
     /** @see Interceptor#onCollectionRemove(Object, Serializable) */
-    public void onCollectionRemove(Object collection, Serializable key) throws CallbackException {
-        // TODO Auto-generated method stub
-        
+    public void onCollectionRemove(final Object collection,
+            final Serializable key) throws CallbackException {
+        // nada que hacer
     }
 
     /** @see Interceptor#onCollectionUpdate(Object, Serializable) */
-    public void onCollectionUpdate(Object collection, Serializable key) throws CallbackException {
-        // TODO Auto-generated method stub
-        
+    public void onCollectionUpdate(final Object collection,
+            final Serializable key) throws CallbackException {
+        // nada que hacer
     }
 
     /** @see Interceptor#onPrepareStatement(String) */
-    public String onPrepareStatement(String sql) {
-        // TODO Auto-generated method stub
+    public String onPrepareStatement(final String sql) {
         return sql;
     }
-
 }
