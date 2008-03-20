ce04785aa43512631ca231666d83aca4cdc1f653
Juan F. Codagnone <juan@zaubersoftware.com>
2008-03-20 20:27:17 +0000

    El hibernate audit logger interceptor de repository ahora hereda de EmptyInterceptor asi no tiene que tener tantos metodos vacios. mejor mantenibilidad.

--- repository/impl/spring-hbm/src/main/java/ar/com/zauber/commons/repository/utils/HibernateAuditLoggerInterceptor.java
+++ repository/impl/spring-hbm/src/main/java/ar/com/zauber/commons/repository/utils/HibernateAuditLoggerInterceptor.java
@@ -8,6 +8,7 @@ import java.util.Date;
 import java.util.Iterator;
 
 import org.hibernate.CallbackException;
+import org.hibernate.EmptyInterceptor;
 import org.hibernate.EntityMode;
 import org.hibernate.Interceptor;
 import org.hibernate.Transaction;
@@ -25,8 +26,8 @@ import ar.com.zauber.commons.repository.ModificationAuditable;
  * 
  * @author Martin Marquez
  */
-public class HibernateAuditLoggerInterceptor implements Interceptor, 
-                                                        InitializingBean {
+public class HibernateAuditLoggerInterceptor extends EmptyInterceptor 
+                                             implements InitializingBean {
 
     private AuthenticationUserMapper<String> authenticationUserMapper;
 
@@ -43,14 +44,6 @@ public class HibernateAuditLoggerInterceptor implements Interceptor,
         }
     }
     
-    /** @see Interceptor#onLoad(Object, Serializable, Object[], String[], Type[]) */
-    public boolean onLoad(final Object arg0, final Serializable arg1,
-            final Object[] arg2, final String[] arg3, final Type[] arg4)
-            throws CallbackException {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
     /**
      * @see Interceptor#onFlushDirty(Object, Serializable, Object[], Object[],
      *      String[], Type[])
@@ -99,101 +92,5 @@ public class HibernateAuditLoggerInterceptor implements Interceptor,
         }
 
         return true;
-    }
-
-    /**
-     * @see Interceptor#onDelete(Object, Serializable, Object[], String[],
-     *      Type[])
-     */
-    public void onDelete(final Object obj, final Serializable id,
-            final Object[] newValues, final String[] properties,
-            final Type[] types) throws CallbackException {
-    }
-
-    /** @see Interceptor#preFlush(Iterator) */
-    public void preFlush(final Iterator arg0) throws CallbackException {
-        // TODO Auto-generated method stub
-    }
-
-    /** @see Interceptor#postFlush(Iterator) */
-    public void postFlush(final Iterator arg0) throws CallbackException {
-        // nada que hacer
-    }
-
-    /**
-     * @see Interceptor#findDirty(Object, Serializable, Object[], Object[],
-     *      String[], Type[])
-     */
-    public int[] findDirty(final Object arg0, final Serializable arg1,
-            final Object[] arg2, final Object[] arg3, final String[] arg4,
-            final Type[] arg5) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    /** @see Interceptor#isTransient(Object) */
-    public Boolean isTransient(final Object arg0) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    /** @see Interceptor#instantiate(String, EntityMode, Serializable) */
-    public Object instantiate(final String arg0, final EntityMode arg1,
-            final Serializable arg2) throws CallbackException {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    /** @see Interceptor#getEntityName(Object) */
-    public String getEntityName(final Object arg0) throws CallbackException {
-        // nada que hacer
-        return null;
-    }
-
-    /** @see Interceptor#getEntity(String, Serializable) */
-    public Object getEntity(final String arg0, final Serializable arg1)
-            throws CallbackException {
-        // nada que hacer
-        return null;
-    }
-
-    /** @see Interceptor#afterTransactionBegin(Transaction) */
-    public void afterTransactionBegin(final Transaction arg0) {
-        // TODO Auto-generated method stub
-    }
-
-    /** @see Interceptor#beforeTransactionCompletion(Transaction) */
-    public void beforeTransactionCompletion(final Transaction arg0) {
-        // TODO Auto-generated method stub
-
-    }
-
-    /** @see Interceptor#afterTransactionCompletion(Transaction) */
-    public void afterTransactionCompletion(final Transaction arg0) {
-        // TODO Auto-generated method stub
-    }
-
-    /** @see Interceptor#onCollectionRecreate(Object, Serializable) */
-    public void onCollectionRecreate(final Object collection,
-            final Serializable key) throws CallbackException {
-        // TODO Auto-generated method stub
-
-    }
-
-    /** @see Interceptor#onCollectionRemove(Object, Serializable) */
-    public void onCollectionRemove(final Object collection,
-            final Serializable key) throws CallbackException {
-        // nada que hacer
-    }
-
-    /** @see Interceptor#onCollectionUpdate(Object, Serializable) */
-    public void onCollectionUpdate(final Object collection,
-            final Serializable key) throws CallbackException {
-        // nada que hacer
-    }
-
-    /** @see Interceptor#onPrepareStatement(String) */
-    public String onPrepareStatement(final String sql) {
-        return sql;
     }
 }
