126c1114dbaa725456ba8f8c2427e38307e591dd
Juan F. Codagnone <juan@zaubersoftware.com>
2008-06-07 23:52:52 +0000

    checkstyle

--- repository/impl/spring-hbm/src/test/java/ar/com/zauber/commons/test/utils/BaseTransactionalRollbackTest.java
+++ repository/impl/spring-hbm/src/test/java/ar/com/zauber/commons/test/utils/BaseTransactionalRollbackTest.java
@@ -15,25 +15,27 @@
  */
 package ar.com.zauber.commons.test.utils;
 
+import org.springframework.test.AbstractSingleSpringContextTests;
 import org.springframework.test.AbstractTransactionalSpringContextTests;
 
+/** test base */
 public class BaseTransactionalRollbackTest
     extends AbstractTransactionalSpringContextTests {
 
-	public BaseTransactionalRollbackTest() {
-		setPopulateProtectedVariables(true);
-	}
-	
-	protected String[] getConfigLocations() {
-		return new String[]  {
-			"classpath:spring-ds.xml",
-            "classpath:spring-hibernate-mappings.xml",
-			"classpath:spring-hibernate.xml",
-            "classpath:spring-repository.xml",
-			"classpath:spring-postprocessing.xml",
-			"classpath:spring-hibernate-drop.xml"
-		};
-	}
-	
+    /** constructor */
+    public BaseTransactionalRollbackTest() {
+        setPopulateProtectedVariables(true);
+    }
 
+    /** @see AbstractSingleSpringContextTests#getConfigLocations() */
+    protected final String[] getConfigLocations() {
+        return new String[]  {
+            "classpath:spring-ds.xml",
+            "classpath:spring-hibernate-mappings.xml",
+            "classpath:spring-hibernate.xml",
+            "classpath:spring-repository.xml",
+            "classpath:spring-postprocessing.xml",
+            "classpath:spring-hibernate-drop.xml"
+        };
+    }
 }
