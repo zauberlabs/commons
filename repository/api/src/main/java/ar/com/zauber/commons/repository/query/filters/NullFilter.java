60bc047c950b824a3c4609f93c239dbf6167e962
Juan F. Codagnone <juan@zaubersoftware.com>
2008-06-07 23:29:36 +0000

    checkstyle

--- repository/api/src/main/java/ar/com/zauber/commons/repository/query/filters/NullFilter.java
+++ repository/api/src/main/java/ar/com/zauber/commons/repository/query/filters/NullFilter.java
@@ -21,14 +21,13 @@ import ar.com.zauber.commons.repository.query.visitor.FilterVisitor;
 /**
  * Clase que representa un filtro vacio (Null Object Pattern)
  * 
- * 
  * @author Martin A. Marquez
  * @since Sep 24, 2007
  */
 public class NullFilter extends BaseFilter {
 
-    /** @see ar.com.zauber.commons.repository.query.filters.Filter#accept(ar.com.zauber.commons.repository.query.visitor.FilterVisitor) */
-    public void accept(FilterVisitor visitor) {
+    /** @see Filter#accept(FilterVisitor) */
+    public void accept(final FilterVisitor visitor) {
         visitor.visitNullFilter(this);
     }
 
