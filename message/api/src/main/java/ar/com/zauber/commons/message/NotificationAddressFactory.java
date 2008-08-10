7bffcf92826a9e33496daae789bb56a847cc98a6
Juan F. Codagnone <juan@zaubersoftware.com>
2008-08-10 13:47:50 +0000

    Nueva interface NotificationAddressFactory que sirve para ocultar la instancia de NotificationAddress que se usa. Esto tiene la ventaja de que el codigo que instancia una notification address no necesaria tiene que depender del modulo -impl.

---
+++ message/api/src/main/java/ar/com/zauber/commons/message/NotificationAddressFactory.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2005-2008 Zauber S.A. <http://www.zauber.com.ar/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ar.com.zauber.commons.message;
+
+/**
+ * Creates {@link NotificationAddress} based on a string. It is usefull
+ * to hide the {@link NotificationAddress} implementation, and also
+ * to manage better the dependencies. 
+ * 
+ * 
+ * @author Juan F. Codagnone
+ * @since Aug 10, 2008
+ */
+public interface NotificationAddressFactory {
+
+    /** 
+     * @return a {@link NotificationAddress} for the string representation.
+     *  it nevers returns null
+     */
+    NotificationAddress createNotificationAddress(String notificationAddress);
+}
