d6330dd665f7ecd48f3df70ec5d8c442d0350d1b
Juan F. Codagnone <juan@zaubersoftware.com>
2008-08-23 00:18:44 +0000

    checkstyle

--- repository/impl/spring-hbm/src/main/java/ar/com/zauber/commons/repository/BaseCreationModificationAuditableEntity.java
+++ repository/impl/spring-hbm/src/main/java/ar/com/zauber/commons/repository/BaseCreationModificationAuditableEntity.java
@@ -26,7 +26,8 @@ import javax.persistence.MappedSuperclass;
  * @since Nov 12, 2007
  */
 @MappedSuperclass
-public abstract class BaseCreationModificationAuditableEntity extends BaseModifiableEntity 
+public abstract class BaseCreationModificationAuditableEntity 
+                           extends BaseModifiableEntity 
                            implements CreationAuditable, ModificationAuditable {
 
     /** <code>createdAt</code> */
