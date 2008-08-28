a6cc4c4f940cef459196ed2ae7205ee9a22520e9
Juan F. Codagnone <juan@zaubersoftware.com>
2008-08-28 23:37:45 +0000

    Issue #: 2862
    commons-web-util:  Add the interface URLRequestMapper:
      Given an HTTP request, the responsability of this class is to return an URL where the resource that can fullfill the request can be found.
      For example a request with the URI /zauber/code/snapshots/ar/com/zauber/maven/pom.xml could be mapped to the url http://localhost:9095/nexus/content/repositories/zauber-code-snapshots/ar/com/zauber/maven/pom.xml 
    
    I have made two implementations of this interface:
      * InmutableURLRequestMapper: always return the same URL
      * PathBasedURLRequestMapper: given a base url (that is acquired using another URLRequestMapper} [for example the InmutableURLRequestMapper appends the paths of the request. It is posible to strip the context path and the servlet path from the request to get the new URL. So, for example, if the request contains the URI /nexusaaa-0.0/bin/doit, and /nexusaaa-0.0  is the  context path, and /bin  is the servlet path, and the base URLRequestMapper used is an InmutableURLRequestMapper  that has the URL http://localhost:9095/nexus the mapper would  resolve the url http://localhost:9095/nexus/doit</tt> (by default the servlet path and the context path are striped)
    
    @worked +1h 30m

---
+++ web/utils/src/main/java/ar/com/zauber/commons/web/proxy/impl/PathBasedURLRequestMapper.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2005-2008 Zauber S.A. <http://www.zauber.com.ar/>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package ar.com.zauber.commons.web.proxy.impl;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.UnhandledException;
+import org.apache.commons.lang.Validate;
+
+import ar.com.zauber.commons.web.proxy.URLRequestMapper;
+
+/**
+ * <p>
+ * {@link URLRequestMapper} that given a base url (that is acquired using
+ *  another {@link URLRequestMapper} [for example the 
+ *  {@link InmutableURLRequestMapper}]) appends the paths of the request.
+ *  It is posible to strip the context path and the servlet path from the request
+ *  to get the new URL.
+ *  </p>  
+ *  <p>
+ *   So, for example, if the request contains the URI
+ *   <tt>/nexusaaa-0.0/bin/doit</tt>, and <tt>/nexusaaa-0.0</tt> is the 
+ *   context path, and <tt>/bin</tt> is the servlet path, and the base
+ *   {@link URLRequestMapper} used is an {@link InmutableURLRequestMapper}
+ *   that has the URL <tt>http://localhost:9095/nexus</tt> the mapper would 
+ *   resolve the url  <tt>http://localhost:9095/nexus/doit</tt> (by default
+ *   the servlet path and the context path are striped).
+ * </p>
+ * 
+ * @author Juan F. Codagnone
+ * @since Aug 28, 2008
+ */
+public class PathBasedURLRequestMapper implements URLRequestMapper {
+    private URLRequestMapper base;
+    private boolean stripContextPath = true;
+    private boolean stripServletPath = true;
+
+    /** constructor */
+    public PathBasedURLRequestMapper(final URLRequestMapper base) {
+        Validate.notNull(base);
+
+        this.base = base;
+    }
+
+    /** @see URLRequestMapper#getProxiedURLFromRequest(HttpServletRequest) */
+    public final URL getProxiedURLFromRequest(final HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        if (stripContextPath) {
+            uri = uri.substring(request.getContextPath().length());
+
+        }
+        if (stripServletPath) {
+            uri = uri.substring(request.getServletPath().length());
+        }
+
+        try {
+            return new URL(base.getProxiedURLFromRequest(request)
+                    .toExternalForm()
+                    + uri);
+        } catch (final MalformedURLException e) {
+            throw new UnhandledException(e);
+        }
+    }
+
+    public final boolean isStripContextPath() {
+        return stripContextPath;
+    }
+
+    /**
+     * <code>true</code> to strip the context path from the context path (for
+     * example application-web-version/)
+     */
+    public final void setStripContextPath(final boolean stripContextPath) {
+        this.stripContextPath = stripContextPath;
+    }
+
+    public final boolean isStripServletPath() {
+        return stripServletPath;
+    }
+
+    /**
+     * <code>true</code> to strip the servlet path from the proxied url (for
+     * example /bin/)
+     */
+    public final void setStripServletPath(final boolean stripServletPath) {
+        this.stripServletPath = stripServletPath;
+    }
+}
---
+++ web/utils/src/test/java/ar/com/zauber/commons/web/proxy/impl/PathBasedURLRequestMapperTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2005-2008 Zauber S.A. <http://www.zauber.com.ar/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ar.com.zauber.commons.web.proxy.impl;
+
+import java.net.URL;
+
+import junit.framework.TestCase;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockServletContext;
+
+/**
+ * Test for {@link PathBasedURLRequestMapper}.
+ * 
+ * 
+ * @author Juan F. Codagnone
+ * @since Aug 28, 2008
+ */
+public class PathBasedURLRequestMapperTest extends TestCase {
+    private final String base = "http://localhost:9095/nexus";
+
+    /** @throws Exception on error */
+    public final void testServletContext() throws Exception {
+        final String ctxPath = "/nexusaaa-0.0";
+        final String servletContext = "/bin";
+
+        final PathBasedURLRequestMapper mapper =  new PathBasedURLRequestMapper(
+                new InmutableURLRequestMapper(new URL(base)));
+        mapper.setStripContextPath(true);
+        mapper.setStripServletPath(true);
+        
+        final MockServletContext ctx = new MockServletContext();
+
+        final MockHttpServletRequest request = new MockHttpServletRequest(ctx,
+                "GET", ctxPath + servletContext + "/");
+        request.setContextPath(ctxPath);
+        request.setServletPath(servletContext);
+        assertEquals(new URL(base + "/"), 
+                mapper.getProxiedURLFromRequest(request));
+    }
+    
+    /** @throws Exception on error */
+    public final void testNoServletContext() throws Exception {
+        final String ctxPath = "/nexusaaa-0.0";
+        final String servletContext = "/bin";
+
+        final PathBasedURLRequestMapper mapper =  new PathBasedURLRequestMapper(
+                new InmutableURLRequestMapper(new URL(base)));
+        mapper.setStripContextPath(false);
+        mapper.setStripServletPath(false);
+        
+        final MockServletContext ctx = new MockServletContext();
+
+        final MockHttpServletRequest request = new MockHttpServletRequest(ctx,
+                "GET", ctxPath + servletContext + "/");
+        request.setContextPath(ctxPath);
+        request.setServletPath(servletContext);
+        assertEquals(new URL(base + ctxPath + servletContext + "/"), 
+                mapper.getProxiedURLFromRequest(request));
+    }
+}
