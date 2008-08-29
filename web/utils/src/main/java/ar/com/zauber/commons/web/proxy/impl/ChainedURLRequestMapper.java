559df8b07c1cb420910f17fa5b8298bb5bd4f737
Juan F. Codagnone <juan@zaubersoftware.com>
2008-08-29 21:35:34 +0000

    change the map of ChainedURLRequestMapper to a property, so it is easier to initialize using spring

--- web/utils/src/main/java/ar/com/zauber/commons/web/proxy/impl/ChainedURLRequestMapper.java
+++ web/utils/src/main/java/ar/com/zauber/commons/web/proxy/impl/ChainedURLRequestMapper.java
@@ -16,9 +16,11 @@
 package ar.com.zauber.commons.web.proxy.impl;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 import java.util.Map.Entry;
 import java.util.regex.Pattern;
 
@@ -57,10 +59,10 @@ public class ChainedURLRequestMapper implements URLRequestMapper {
     }
     
     /** @see #getChain(Map) */
-    public ChainedURLRequestMapper(final Map<String, String> map) {
+    public ChainedURLRequestMapper(final Properties map) {
         this(getChain(map));
     }
-
+    
     /**
      * given a map of strings, builds a chain of {@link RegexURLRequestMapper}.
      * <pre>
@@ -68,11 +70,13 @@ public class ChainedURLRequestMapper implements URLRequestMapper {
      *  ^/nexus/(.*)$ = http://localhost:9095/nexus/$1 [^] [^]
      *  ^/([^/]+)/([^/]+)/([^/]+)/(.*)$ = http://localhost:9095/.../$1-$2-$3/$4 
      */
-    private static List<URLRequestMapper> getChain(final Map<String, String> m) {
-        final Map<String, String> map = new LinkedHashMap<String, String>(m);
-        final List<URLRequestMapper> l = new ArrayList<URLRequestMapper>();
+    private static List<URLRequestMapper> getChain(
+            final Hashtable map) {
         Validate.notNull(map);
-        for(final Entry<String, String> entry : map.entrySet()) {
+        
+        final List<URLRequestMapper> l = new ArrayList<URLRequestMapper>();
+        for(final Entry<String, String> entry 
+            : ((Set< Entry<String, String>>)map.entrySet())) {
             l.add(new RegexURLRequestMapper(Pattern.compile(entry.getKey()), 
                     entry.getValue()));
         }
