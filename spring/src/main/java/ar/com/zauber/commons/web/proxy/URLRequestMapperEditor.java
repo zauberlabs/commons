3c091f0935bf2cc85399a93ea49794f85714a762
Juan F. Codagnone <juan@zaubersoftware.com>
2008-08-29 22:44:19 +0000

    Issue #: 2862
    PropertyEditor to ease the initialization of Chained URLRequestMappers.
    
    Example:
    
    <bean name='/**' class="ar.com.zauber.commons.spring.web.controllers.ProxyController">
            <constructor-arg index="0" value="user agent"/>
            <constructor-arg index="1">
                <value> 
                  ^/nexus/public/(.*)$=http://localhost:9095/nexus/content/repositories/public/$1
                  ^/nexus/nexus/(.*)$=http://localhost:9095/nexus/$1
                  ^/nexus/([^/]+)/([^/]+)/([^/]+)/(.*)$=http://localhost:9095/nexus/content/repositories/$1-$2-$3/$4
                  ^/hudson/(.*)$=http://localhost:8586/hudson/$1
                </value>
            </constructor-arg>
    </bean> 
    
    This line, and those below, will be ignored--
    
    A    spring/src/test/java/ar/com/zauber/commons/web
    A    spring/src/test/java/ar/com/zauber/commons/web/proxy
    AM   spring/src/test/java/ar/com/zauber/commons/web/proxy/URLRequestMapperEditorTest.java
    A    spring/src/main/java/ar/com/zauber/commons/web
    A    spring/src/main/java/ar/com/zauber/commons/web/proxy
    AM   spring/src/main/java/ar/com/zauber/commons/web/proxy/URLRequestMapperEditor.java

---
+++ spring/src/main/java/ar/com/zauber/commons/web/proxy/URLRequestMapperEditor.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2005-2008 Zauber S.A. <http://www.zauber.com.ar/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ar.com.zauber.commons.web.proxy;
+
+import java.beans.PropertyEditorSupport;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.UnhandledException;
+
+import ar.com.zauber.commons.web.proxy.impl.ChainedURLRequestMapper;
+
+/**
+ * {@link PropertyEditor} for {@link URLRequestMapper}.
+ * 
+ * Creates a {@link ChainedURLRequestMapper} of {@link RegexURLRequestMapper}.
+ * @see ChainedURLRequestMapper#ChainedURLRequestMapper(Map).
+ * 
+ * @author Juan F. Codagnone
+ * @since Aug 29, 2008
+ */
+public class URLRequestMapperEditor extends PropertyEditorSupport {
+
+    /** @see PropertyEditor#setAsText(String) */
+    public final void setAsText(final String s) 
+        throws IllegalArgumentException {
+        URLRequestMapper r = null; 
+        
+        if(!StringUtils.isBlank(s)) {
+            final Map<String, String> m = new LinkedHashMap<String, String>();
+            final BufferedReader br = new BufferedReader(new StringReader(s));
+            String l;
+            try {
+                while((l = br.readLine()) != null) {
+                    if(StringUtils.isBlank(l)) {
+                        continue;
+                    }
+                    final int i = l.indexOf("=");
+                    if(i == -1) {
+                        throw new IllegalArgumentException("missing = in line `"
+                                + l + "'");
+                    }
+                    m.put(l.substring(0, i).trim(), l.substring(i + 1).trim());
+                }
+                r = new ChainedURLRequestMapper(m);
+            } catch (final IOException e) {
+                throw new UnhandledException(e);
+            }
+        }
+        setValue(r);
+    }
+}
