7e83dceb99310e9280b99629271d5899e6662d74
Juan F. Codagnone <juan@zaubersoftware.com>
2008-08-29 19:48:52 +0000

    Issue #: 2862
    URLRequestMapper that chains multiples URLRequestMapper. Tries to resolve a
    destination url with a list of URLRequestMapper. Tries until on of the chain
    returns a value.
    
    This is usefull with the RegexURLRequestMapper. You can achive cofigurations
    like:
    
      ^/public/(.*)$  = http://localhost:9095/nexus/content/repositories/public/$1
      ^/nexus/(.*)$   = http://localhost:9095/nexus/$1 [^]
      ^/([^/]+)/([^/]+)/([^/]+)/(.*)$ = http://localhost:9095/nexus/content/repositories/$1-$2-$3/$4

---
+++ web/utils/src/main/java/ar/com/zauber/commons/web/proxy/impl/ChainedURLRequestMapper.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2005-2008 Zauber S.A. <http://www.zauber.com.ar/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ar.com.zauber.commons.web.proxy.impl;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.Validate;
+
+import ar.com.zauber.commons.web.proxy.URLRequestMapper;
+import ar.com.zauber.commons.web.proxy.URLResult;
+
+/**
+ * <p>
+ * {@link URLRequestMapper} that chains multiples URLRequestMapper. Tries
+ * to resolve a destination url with a list of {@link URLRequestMapper}. 
+ * Tries until on of the chain returns a value.
+ * </p><p>
+ * This is usefull with the {@link RegexURLRequestMapper}.
+ * You can achive cofigurations like:
+ * <pre>
+ *  ^/public/(.*)$  = http://localhost:9095/nexus/content/repositories/public/$1
+ *  ^/nexus/(.*)$   = http://localhost:9095/nexus/$1 [^]
+ *  ^/([^/]+)/([^/]+)/([^/]+)/(.*)$ = http://localhost:9095/nexus/content/repositories/$1-$2-$3/$4
+ * </pre>
+ * </p>
+ * @author Juan F. Codagnone
+ * @since Aug 29, 2008
+ */
+public class ChainedURLRequestMapper implements URLRequestMapper {
+    private final List<URLRequestMapper> chain;
+
+    /** Creates the ChainedURLRequestMapper. */
+    public ChainedURLRequestMapper(final List<URLRequestMapper> chain) {
+        Validate.noNullElements(chain);
+        
+        this.chain = chain;
+    }
+    
+    /** @see URLRequestMapper#getProxiedURLFromRequest(HttpServletRequest) */
+    public final URLResult getProxiedURLFromRequest(
+            final HttpServletRequest request) {
+        URLResult ret = new InmutableURLResult();
+        for(final URLRequestMapper urlRequestMapper : chain) {
+            ret = urlRequestMapper.getProxiedURLFromRequest(request);
+            if(ret.hasResult()) {
+                break;
+            }
+        }
+        
+        return ret;
+    }
+}
---
+++ web/utils/src/test/java/ar/com/zauber/commons/web/proxy/impl/ChainedURLRequestMapperTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2005-2008 Zauber S.A. <http://www.zauber.com.ar/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ar.com.zauber.commons.web.proxy.impl;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import junit.framework.TestCase;
+import ar.com.zauber.commons.web.proxy.URLRequestMapper;
+import ar.com.zauber.commons.web.proxy.URLResult;
+
+/**
+ * Tests for {@link ChainedURLRequestMapper}
+ * 
+ * 
+ * @author Juan F. Codagnone
+ * @since Aug 29, 2008
+ */
+public class ChainedURLRequestMapperTest extends TestCase {
+
+    /** test */
+    public final void testEmpty()  {
+        final URLRequestMapper c = new ChainedURLRequestMapper(
+                Collections.EMPTY_LIST);
+        c.getProxiedURLFromRequest(new MockHttpServletRequest());
+    }
+    
+
+    /** test */
+    public final void testNullArguments()  {
+        try {
+            new ChainedURLRequestMapper(Arrays.asList(new URLRequestMapper[]{
+                new InmutableURLRequestMapper(new InmutableURLResult()),
+                null,
+            }));
+            fail();
+        } catch(final IllegalArgumentException e) {
+            // ok
+        }
+    }
+    
+    /**   @throws MalformedURLException on test */
+    public final void testChain() throws MalformedURLException  {
+        final URLRequestMapper c = new ChainedURLRequestMapper(
+            Arrays.asList(new URLRequestMapper[] {
+                new RegexURLRequestMapper(
+                        new InmutableURLRequestMapper(
+                                new InmutableURLResult(new URL(
+                         "http://localhost:9095/nexus/"))),
+                        Pattern.compile("^/nexus/(.*)$"),
+                        "$1"),
+                new RegexURLRequestMapper(
+                        new InmutableURLRequestMapper(
+                                new InmutableURLResult(new URL(
+                         "http://localhost:9095/nexus/content/repositories/"))),
+                        Pattern.compile("^/([^/]+)/([^/]+)/([^/]+)/(.*)$"),
+                        "$1-$2-$3/$4"),
+        }));
+        
+       assertFalse(c.getProxiedURLFromRequest(new MockHttpServletRequest(
+               "GET", "/foo")).hasResult());
+       assertEquals(new URL("http://localhost:9095/nexus/foo/bar"), 
+               c.getProxiedURLFromRequest(new MockHttpServletRequest(
+               "GET", "/nexus/foo/bar")).getURL());
+       assertEquals(new URL("http://localhost:9095/nexus/content/repositories/"
+               + "zauber-code-releases/foo/bar"), 
+               c.getProxiedURLFromRequest(new MockHttpServletRequest(
+               "GET", "/zauber/code/releases/foo/bar")).getURL()); 
+    }
+}
