9b93b1460580d32e693580ca17fba00fb97c6344
Juan F. Codagnone <juan@zaubersoftware.com>
2009-01-05 17:07:25 +0000

    Issue #: 3930
    copyright headers updates for commons

--- spring/src/main/java/ar/com/zauber/commons/spring/web/utils/ResourcePropertiesVersionProvider.java
+++ spring/src/main/java/ar/com/zauber/commons/spring/web/utils/ResourcePropertiesVersionProvider.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (c) 2005-2008 Zauber S.A. <http://www.zauber.com.ar/>
+/**
+ * Copyright (c) 2005-2009 Zauber S.A. <http://www.zauber.com.ar/>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package ar.com.zauber.commons.spring.web.utils;
 
 import org.springframework.core.io.Resource;
