8fc355e8f6ece7a643fceffaa9e9be41107c281a
Juan F. Codagnone <juan@zaubersoftware.com>
2009-06-19 15:54:02 +0000

    me habia quedado vacio el test del one-way-merge

--- dao/src/test/java/ar/com/zauber/commons/dao/impl/OneWayMergeClosureProcessorTest.java
+++ dao/src/test/java/ar/com/zauber/commons/dao/impl/OneWayMergeClosureProcessorTest.java
@@ -15,14 +15,137 @@
  */
 package ar.com.zauber.commons.dao.impl;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.apache.commons.lang.Validate;
+import org.junit.Test;
+
+import ar.com.zauber.commons.dao.Closure;
+import ar.com.zauber.commons.dao.ClosureProcessor;
+import ar.com.zauber.commons.dao.impl.MergeResult.Operation;
+
 
 /**
- * TODO Descripcion de la clase. Los comenterios van en castellano.
- * 
+ * Test {@link OneWayMergeClosureProcessor}
  * 
  * @author Juan F. Codagnone
  * @since Jun 19, 2009
  */
 public class OneWayMergeClosureProcessorTest {
 
+    /** lo ideal lleno, nada en un lado */
+    @Test
+    public final void allLeft() {
+        final ClosureProcessor<MergeResult<String>> p = 
+            new OneWayMergeClosureProcessor<String>(Arrays.asList(
+                    new String[]{}), 
+                    Arrays.asList(new String[]{"juan", "pedro"}));
+        
+        final List<MergeResult<String>> expected = 
+            new LinkedList<MergeResult<String>>();
+        expected.add(new InmutableMergeResult<String>(Operation.ADD, "juan"));
+        expected.add(new InmutableMergeResult<String>(Operation.ADD, "pedro"));
+        final MergeTestClosure closure = new MergeTestClosure(expected.iterator()); 
+        p.process(closure);
+        closure.assertAll();
+    }
+    
+    /** lo ideal lleno, nada en un lado */
+    @Test
+    public final void allMergeAdd() {
+        final ClosureProcessor<MergeResult<String>> p = 
+            new OneWayMergeClosureProcessor<String>(Arrays.asList(
+                    new String[]{"juan", "pedro"}), 
+                    Arrays.asList(new String[]{"juan", "jose", "pedro", }));
+        
+        final List<MergeResult<String>> expected = 
+            new LinkedList<MergeResult<String>>();
+        expected.add(new InmutableMergeResult<String>(Operation.KEEP, "juan"));
+        expected.add(new InmutableMergeResult<String>(Operation.ADD, "jose"));
+        expected.add(new InmutableMergeResult<String>(Operation.KEEP, "pedro"));
+        final MergeTestClosure closure = new MergeTestClosure(expected.iterator()); 
+        p.process(closure);
+        closure.assertAll();
+    }
+    
+    /** lo ideal lleno, nada en un lado */
+    @Test
+    public final void allMergeRemove() {
+        final ClosureProcessor<MergeResult<String>> p = 
+            new OneWayMergeClosureProcessor<String>(Arrays.asList(
+                    new String[]{"juan", "jose", "pedro"}), 
+                    Arrays.asList(new String[]{"juan", "pedro", }));
+        
+        final List<MergeResult<String>> expected = 
+            new LinkedList<MergeResult<String>>();
+        expected.add(new InmutableMergeResult<String>(Operation.KEEP, "juan"));
+        expected.add(new InmutableMergeResult<String>(Operation.REMOVE, "jose"));
+        expected.add(new InmutableMergeResult<String>(Operation.KEEP, "pedro"));
+        final MergeTestClosure closure = new MergeTestClosure(expected.iterator()); 
+        p.process(closure);
+        closure.assertAll();
+    }
+    
+    /** lo ideal lleno, nada en un lado */
+    @Test
+    public final void allRemove() {
+        final ClosureProcessor<MergeResult<String>> p = 
+            new OneWayMergeClosureProcessor<String>(Arrays.asList(
+                    new String[]{"juan", "jose", "pedro"}), 
+                    Arrays.asList(new String[]{}));
+        
+        final List<MergeResult<String>> expected = 
+            new LinkedList<MergeResult<String>>();
+        expected.add(new InmutableMergeResult<String>(Operation.REMOVE, "juan"));
+        expected.add(new InmutableMergeResult<String>(Operation.REMOVE, "jose"));
+        expected.add(new InmutableMergeResult<String>(Operation.REMOVE, "pedro"));
+        final MergeTestClosure closure = new MergeTestClosure(expected.iterator()); 
+        p.process(closure);
+        closure.assertAll();
+    }
+}
+
+/**
+ * Prueba que un closure recibe lo que debe recibir 
+ * @author Juan F. Codagnone
+ * @since Jun 19, 2009
+ */
+class MergeTestClosure implements Closure<MergeResult<String>> {
+    private final Iterator<MergeResult<String>> expected;
+    private MergeResult<String> current;
+    
+    
+    /** Creates the TestClosure. */
+    public MergeTestClosure(final Iterator<MergeResult<String>> expected) {
+        Validate.notNull(expected);
+        
+        if(expected.hasNext()) {
+            current = expected.next();
+        }
+        this.expected = expected;
+    }
+    
+    /** @see Closure#execute(Object) */
+    public void execute(final MergeResult<String> result) {
+        Assert.assertTrue(current != null);
+        Assert.assertTrue(result != null);
+        
+        Assert.assertEquals(current.getOperation(), result.getOperation());
+        Assert.assertEquals(current.getEntity(), result.getEntity());
+        
+        if(expected.hasNext()) {
+            current = expected.next();
+        }
+    }
+    
+    /** se procesaron todas las cosas que se esperaba? */
+    public void assertAll() {
+        Assert.assertTrue(!expected.hasNext());
+    }
+    
 }
