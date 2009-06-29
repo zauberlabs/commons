a70cb543e116c96ad462ece1d02109fe2e8d3284
Pablo Martín Grigolatto <pablog@zaubersoftware.com>
2009-06-29 18:38:37 +0000

    Un controller para manejar errores en forma generica

---
+++ spring/src/main/java/ar/com/zauber/commons/spring/web/controllers/ExceptionController.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2005-2009 Zauber S.A. <http://www.zauber.com.ar/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ar.com.zauber.commons.spring.web.controllers;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.Validate;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+/**
+ * Controlador que mapea condiciones de error 
+ * 
+ * @author Pablo Grigolatto
+ * @since Jun 29, 2009
+ */
+public class ExceptionController extends AbstractController {
+    private final Map<Integer, String> map;
+    private final String defaultView;
+
+    /** Creates the ExceptionController.*/
+    public ExceptionController(final Map<Integer, String> map, 
+            final String defaultView) {
+        Validate.notNull(map);
+        Validate.isTrue(StringUtils.isNotBlank(defaultView));
+        this.map = map;
+        this.defaultView = defaultView;
+    }
+
+    /** @see AbstractController#handleRequestInternal(
+     * HttpServletRequest, HttpServletResponse) */
+    @Override
+    protected final ModelAndView handleRequestInternal(
+            final HttpServletRequest request,
+            final HttpServletResponse response) throws Exception {
+
+        final String view;
+        final Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        
+        if(map.containsKey(statusCode)) {
+            view = map.get(statusCode);
+        } else {
+            view = this.defaultView;
+        }
+
+        return new ModelAndView(view);
+    }
+
+}
---
+++ spring/src/test/java/ar/com/zauber/commons/spring/web/controllers/ExceptionControllerTest.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2005-2009 Zauber S.A. <http://www.zauber.com.ar/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ar.com.zauber.commons.spring.web.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Pruebas para {@link ExceptionController}
+ * 
+ * @author Pablo Grigolatto
+ * @since Jun 29, 2009
+ */
+public class ExceptionControllerTest {
+    private ExceptionController exceptionController; 
+    private HttpServletResponse res;
+    private HttpServletRequest req;
+    
+    @Before
+    public final void before() {
+        res = new MockHttpServletResponse();
+        req = new MockHttpServletRequest();
+        
+        final Map<Integer, String> map = new HashMap<Integer, String>();
+            map.put(404, "exceptions/notfound");
+            map.put(500, "exceptions/internalerror");
+        
+        exceptionController = new ExceptionController(map, "exceptions/default");
+    }
+
+    @Test
+    public final void test404() throws Exception{
+        req.setAttribute("javax.servlet.error.status_code", 404);
+        final ModelAndView mav = exceptionController.handleRequestInternal(req, res);
+        Assert.assertEquals("exceptions/notfound", mav.getViewName());
+    }
+    
+    @Test
+    public final void test500() throws Exception{
+        req.setAttribute("javax.servlet.error.status_code", 500);
+        final ModelAndView mav = exceptionController.handleRequestInternal(req, res);
+        Assert.assertEquals("exceptions/internalerror", mav.getViewName());
+    }
+    
+    @Test
+    public final void testNull() throws Exception{
+        final ModelAndView mav = exceptionController.handleRequestInternal(req, res);
+        Assert.assertEquals("exceptions/default", mav.getViewName());
+    }
+    
+}
