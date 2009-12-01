9d87502459e13a2ad4d58cf342dfcc7254dd1d47
Mariano Semelman <marianosemelman@zaubersoftware.com>
2009-12-01 15:01:07 +0000

    se cambio el metodo parse para que reciba un inputSource (es mas universal) se piensa deprecar el parse con inputSource

--- web/transformation/src/main/java/ar/com/zauber/commons/web/transformation/processors/impl/DocumentBuilderFactoryDocumentProvider.java
+++ web/transformation/src/main/java/ar/com/zauber/commons/web/transformation/processors/impl/DocumentBuilderFactoryDocumentProvider.java
@@ -30,42 +30,47 @@ import javax.xml.transform.stream.StreamResult;
 import org.apache.commons.lang.UnhandledException;
 import org.apache.commons.lang.Validate;
 import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import ar.com.zauber.commons.web.transformation.processors.DocumentProvider;
 
 /**
  * Provee documentos DOMs
- * 
+ *
  * @author Juan F. Codagnone
  * @since May 30, 2009
  */
 public class DocumentBuilderFactoryDocumentProvider implements DocumentProvider {
     // Using factory get an instance of document builder
     private final DocumentBuilderFactory dbf;
-    
+
     /** Creates the DocumentBuilderFactoryDocumentProvider. */
     public DocumentBuilderFactoryDocumentProvider() {
         this(DocumentBuilderFactory.newInstance());
         dbf.setNamespaceAware(true);
     }
-    
+
     /** Creates the DocumentBuilderFactoryDocumentProvider. */
     public DocumentBuilderFactoryDocumentProvider(
             final DocumentBuilderFactory dbf) {
         Validate.notNull(dbf);
-        
+
         this.dbf = dbf;
     }
 
-    /** @see DocumentProvider#getInputStream(InputStream) */
+    /** @see DocumentProvider#getInputStream(InputStream)
+     *  deprecado utilizar
+     *  {@link DocumentBuilderFactoryDocumentProvider#parse(InputSource)}*/
+    @Deprecated
     public final Document parse(final InputStream inputStream) {
-        
-        
+        return parse(new InputSource(inputStream));
+    }
+
+    public final Document parse(InputSource source) {
         try {
             final DocumentBuilder db = dbf.newDocumentBuilder();
-            // parse using builder to get DOM representation of the XML file
-            return db.parse(inputStream);
+            return db.parse(source);
         } catch (ParserConfigurationException e) {
             throw new UnhandledException(e);
         } catch (SAXException e) {
@@ -79,7 +84,7 @@ public class DocumentBuilderFactoryDocumentProvider implements DocumentProvider 
     public final void serialize(final Document document, final OutputStream os) {
         try {
             TransformerFactory.newInstance().newTransformer().transform(
-                    new DOMSource(document), new StreamResult(os));  
+                    new DOMSource(document), new StreamResult(os));
             os.flush();
         } catch(IOException e) {
             throw new UnhandledException(e);
