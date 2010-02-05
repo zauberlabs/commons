3b0a27e75e6df3e83fe962f055269acb3c1eb326
Francisco Gonzalez <francisco@zaubersoftware.com>
2010-02-05 21:05:06 +0000

    Twitter4JOAuthAccessToken ahora implementa Serializable

--- social/oauth/twitter/src/main/java/ar/com/zauber/commons/social/oauth/twitter/Twitter4JOAuthAccessToken.java
+++ social/oauth/twitter/src/main/java/ar/com/zauber/commons/social/oauth/twitter/Twitter4JOAuthAccessToken.java
@@ -1,7 +1,9 @@
 /*
- * Copyright (c) 2010 Zauber S.A.  -- All rights reserved
+ * Copyright (c) 2010 Zauber S.A. -- All rights reserved
  */
 package ar.com.zauber.commons.social.oauth.twitter;
+
+import java.io.Serializable;
 
 import org.apache.commons.lang.Validate;
 
@@ -14,23 +16,26 @@ import ar.com.zauber.commons.social.oauth.OAuthAccessToken;
  * @author Francisco J. González Costanzó
  * @since Feb 4, 2010
  */
-public class Twitter4JOAuthAccessToken implements OAuthAccessToken {
+public class Twitter4JOAuthAccessToken implements OAuthAccessToken,
+        Serializable {
+    /** <code>serialVersionUID</code> */
+    private static final long serialVersionUID = 5018916311032391476L;
     
     private final AccessToken accessToken;
 
     /** Creates the Twitter4JOAuthAccessToken. */
-    public Twitter4JOAuthAccessToken(AccessToken accessToken) {
+    public Twitter4JOAuthAccessToken(final AccessToken accessToken) {
         Validate.notNull(accessToken);
         this.accessToken = accessToken;
     }
 
     /** @see OAuthAccessToken#getToken() */
-    public String getToken() {
+    public final String getToken() {
         return accessToken.getToken();
     }
 
     /** @see OAuthAccessToken#getTokenSecret() */
-    public String getTokenSecret() {
+    public final String getTokenSecret() {
         return accessToken.getTokenSecret();
     }
 
