4f0faf4aba8d61ef32fb5ecf4ecf40ec2a11ea95
Pablo Martín Grigolatto <pablog@zaubersoftware.com>
2010-03-19 18:25:06 +0000

    Agrego un DateProvider que permite truncar la fecha por un campo específico.

---
+++ commons/src/main/java/ar/com/zauber/commons/date/impl/SignificantDateProvider.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2005-2010 Zauber S.A. <http://www.zauber.com.ar/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ar.com.zauber.commons.date.impl;
+
+import java.util.Date;
+
+import org.apache.commons.lang.Validate;
+import org.apache.commons.lang.time.DateUtils;
+
+import ar.com.zauber.commons.date.DateProvider;
+
+/**
+ * Recorta la fecha luego del último digito significativo.
+ * Por ejemplo: para 2010-03-03 19:54:22 podria retornar 2010-03-03 00:00:00
+ * 
+ * @author Pablo Grigolatto
+ * @since Mar 19, 2010
+ */
+public class SignificantDateProvider implements DateProvider {
+
+    private final DateProvider target;
+    private final int field;
+
+    /** Creates the CurrentSignificantDayDateProvider. */
+    public SignificantDateProvider(
+            final DateProvider target, final int field) {
+        Validate.notNull(target);
+        this.target = target;
+        this.field = field;
+    }
+    
+    /** @see DateProvider#getDate() */
+    public final Date getDate() {
+        return DateUtils.truncate(target.getDate(), field);
+    }
+
+}
---
+++ commons/src/test/java/ar/com/zauber/commons/date/SignificantDateProviderTest.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2005-2010 Zauber S.A. <http://www.zauber.com.ar/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ar.com.zauber.commons.date;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import junit.framework.Assert;
+
+import org.apache.commons.lang.time.DateUtils;
+import org.junit.Test;
+
+import ar.com.zauber.commons.date.impl.CurrentDateProvider;
+import ar.com.zauber.commons.date.impl.SignificantDateProvider;
+
+/**
+ * Pruebas sobre {@link SignificantDateProvider}
+ * 
+ * @author Pablo Grigolatto
+ * @since Mar 19, 2010
+ */
+public class SignificantDateProviderTest {
+
+    /** dia del mes */
+    @Test
+    public final void testDay() {
+        final DateProvider provider = new SignificantDateProvider(
+                new CurrentDateProvider(), Calendar.DAY_OF_MONTH);
+        Assert.assertEquals(
+                DateUtils.truncate(new Date(), Calendar.DAY_OF_MONTH), 
+                provider.getDate());
+    }
+    
+    /** hora del dia */
+    @Test
+    public final void testHour() {
+        final DateProvider provider = new SignificantDateProvider(
+                new CurrentDateProvider(), Calendar.HOUR_OF_DAY);
+        Assert.assertEquals(
+                DateUtils.truncate(new Date(), Calendar.HOUR_OF_DAY), 
+                provider.getDate());
+    }
+    
+}
