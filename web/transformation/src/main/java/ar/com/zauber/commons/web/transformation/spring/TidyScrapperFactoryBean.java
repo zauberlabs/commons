953dd76fdd9cdf4455ba4fe6206e6381ed5cfdb3
Juan F. Codagnone <juan@zaubersoftware.com>
2010-03-20 17:30:49 +0000

    Factory bean para crear menos verbarragicamente TidyScrappers

---
+++ web/transformation/src/main/java/ar/com/zauber/commons/web/transformation/spring/TidyScrapperFactoryBean.java
@@ -0,0 +1,68 @@
+/**
+ *  Copyright (c) 2010 CMD S.A.  -- All rights reserved
+ */
+package ar.com.zauber.commons.web.transformation.spring;
+
+import java.io.InputStream;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.dom.DOMSource;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.Validate;
+import org.springframework.beans.factory.FactoryBean;
+import org.xml.sax.InputSource;
+
+import ar.com.zauber.commons.web.transformation.processors.DocumentProvider;
+import ar.com.zauber.commons.web.transformation.processors.impl.TidyScrapper;
+import ar.com.zauber.commons.web.transformation.processors.impl.XalanXSLTScraper;
+
+/**
+ * Crea mas facilmente scrappers. Ej
+ * <verbatim>
+ * <bean name="categoryListScrapper" class="...TidyScrapperFactoryBean">
+ *       <description>
+ *           Scrapper para paginas como http://.../foo/bar
+ *       </description>
+ *       <constructor-arg index="0" value="com/foo/bar/trans/categorylist.xsl"/>
+ *       <constructor-arg index="1" ref="htmlProvider"/>
+ *       <constructor-arg index="2" ref="xmlProvider"/>
+ *   </bean>
+ * </verbatim>
+ * @author Juan F. Codagnone
+ * @since Jan 25, 2010
+ */
+public class TidyScrapperFactoryBean implements FactoryBean<TidyScrapper> {
+    private final TidyScrapper tidyScrapper;
+    
+    /** Creates the TidyScrapperFactoryBean. */
+    public TidyScrapperFactoryBean(final String path, 
+            final DocumentProvider jtidy, final DocumentProvider xml) {
+        Validate.notNull(jtidy);
+        Validate.notNull(xml);
+
+        final InputStream is = getClass().getClassLoader().getResourceAsStream(path);
+        try {
+            final Source s1 =
+                new DOMSource(xml.parse(new InputSource(is)));
+            tidyScrapper = new TidyScrapper(jtidy, new XalanXSLTScraper(s1));
+        } finally {
+            IOUtils.closeQuietly(is);
+        }
+    }
+    
+    /** @see FactoryBean#getObject() */
+    public final TidyScrapper getObject() throws Exception {
+        return tidyScrapper;
+    }
+
+    /** @see FactoryBean#getObjectType() */
+    public final Class<? extends TidyScrapper> getObjectType() {
+        return tidyScrapper.getClass();
+    }
+
+    /** @see FactoryBean#isSingleton() */
+    public final boolean isSingleton() {
+        return true;
+    }
+}
