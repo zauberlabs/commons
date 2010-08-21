f76e1fde9a5caea3e5ca9e9c7cf64cafacd696f7
Juan F. Codagnone <juan@zaubersoftware.com>
2010-08-21 22:40:34 +0000

    generalizo

--- base/src/main/java/ar/com/zauber/commons/dao/predicate/EqualsPredicate.java
+++ base/src/main/java/ar/com/zauber/commons/dao/predicate/EqualsPredicate.java
@@ -25,18 +25,19 @@ import ar.com.zauber.commons.dao.Predicate;
  * 
  * @author Juan F. Codagnone
  * @since Jul 12, 2008
+ * @param <T> type
  */
-public class EqualsPredicate implements Predicate<String> {
-    private final String target;
+public class EqualsPredicate<T> implements Predicate<T> {
+    private final T target;
 
     /** Creates the EqualsPredicate. */
-    public EqualsPredicate(final String target) {
+    public EqualsPredicate(final T target) {
         Validate.notNull(target);
         this.target = target;
     }
     
-    /** @see Predicate#evaluate(java.lang.Object) */
-    public final boolean evaluate(final String value) {
+    /** @see Predicate#evaluate(Object) */
+    public final boolean evaluate(final T value) {
         return target.equals(value);
     }
 
