d931723d89210329ba63bbc59f9a42065e1492f3
Marcelo Turrin <marcelo@zaubersoftware.com>
2010-09-13 21:56:51 +0000

    Agrego usertype para las uris, asi a la base de datos no va la uri serializada sino la representación en forma de string

---
+++ persistence/utils/src/main/java/ar/com/zauber/commons/repository/utils/URIUserType.java
@@ -0,0 +1,169 @@
+/**
+ * Copyright (c) 2005-2010 Zauber S.A. <http://www.zauber.com.ar/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ar.com.zauber.commons.repository.utils;
+
+import java.io.Serializable;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import org.apache.commons.lang.Validate;
+import org.hibernate.HibernateException;
+import org.hibernate.type.SerializationException;
+import org.hibernate.usertype.UserType;
+
+
+/**
+ * Sabe serializar {@link URI}s como columnas de texto
+ * 
+ * @author Marcelo Turrin
+ * @since Jun 22, 2009
+ */
+public class URIUserType implements UserType {
+
+    private static final int[] SQL_TYPE = {Types.CLOB };
+
+    /**
+     * @see UserType#equals(Object, Object)
+     * @throws HibernateException
+     *             on error
+     */
+    public final boolean equals(final Object x, final Object y)
+            throws HibernateException {
+        boolean ret = false;
+        if (x == y) {
+            ret = true;
+        } else if (null == x || null == y) {
+            ret = false;
+        } else {
+            ret = x.equals(y);
+        }
+
+        return ret;
+    }
+
+    /**
+     * @see usertype.UserType#hashCode(Object)
+     * @throws HibernateException
+     *             on error
+     */
+    public final int hashCode(final Object x) throws HibernateException {
+        return x.hashCode();
+    }
+
+    /** @see UserType#isMutable() */
+    public final boolean isMutable() {
+        return false;
+    }
+
+    /** @see UserType#returnedClass() */
+    @SuppressWarnings("unchecked")
+    public final Class returnedClass() {
+        return URI.class;
+    }
+
+    /** @see UserType#sqlTypes() */
+    public final int[] sqlTypes() {
+        return SQL_TYPE;
+    }
+
+    /**
+     * @see UserType#nullSafeGet(ResultSet, String[], Object)
+     * @throws HibernateException
+     *             on error
+     * @throws SQLException
+     *             on error
+     */
+    public final Object nullSafeGet(final ResultSet rs, final String[] names,
+            final Object owner) throws HibernateException, SQLException {
+        Validate.notNull(rs);
+        Validate.notNull(names);
+        Validate.isTrue(names.length > 0);
+        final String uriString = rs.getString(names[0]);
+        Object ret = null;
+        if (uriString != null) {
+            try {
+                ret = new URI(uriString);
+            } catch (final URISyntaxException ex) {
+                throw new SerializationException("el texto `" + uriString
+                        + "' no es una URI valida", ex);
+            }
+        }
+        return ret;
+    }
+
+    /**
+     * @see UserType#nullSafeSet(PreparedStatement, Object, int)
+     * @throws HibernateException
+     *             on error
+     * @throws SQLException
+     *             on error
+     */
+    public final void nullSafeSet(final PreparedStatement st,
+            final Object value, final int index) throws HibernateException,
+            SQLException {
+        Validate.notNull(st);
+        Validate.isTrue(value == null || value instanceof URI);
+        if (value == null) {
+            st.setNull(index, Types.VARCHAR);
+        } else {
+            st.setString(index, ((URI)value).toString());
+        }
+
+    }
+
+    /**
+     * @see UserType#assemble(Serializable, Object)
+     * @throws HibernateException
+     *             on error
+     */
+    public final Object assemble(final Serializable cached, final Object owner)
+            throws HibernateException {
+        return cached;
+    }
+
+    /**
+     * @see UserType#deepCopy(java.lang.Object)
+     * @throws HibernateException
+     *             on error
+     */
+    public final Object deepCopy(final Object value) throws HibernateException {
+        return value;
+    }
+
+    /**
+     * @see UserType#disassemble(java.lang.Object)
+     * @throws HibernateException
+     *             on error
+     */
+    public final Serializable disassemble(final Object value)
+            throws HibernateException {
+        return (Serializable)value;
+    }
+
+    /**
+     * @see UserType#replace(Object, Object, Object) *
+     * @throws HibernateException
+     *             on error
+     */
+    public final Object replace(final Object original, final Object target,
+            final Object owner) throws HibernateException {
+        return original;
+    }
+}
---
+++ persistence/utils/src/test/java/ar/com/zauber/commons/repository/utils/URIUserTypeTest.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) 2010 Zauber S.A.  -- All rights reserved
+ */
+package ar.com.zauber.commons.repository.utils;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import junit.framework.Assert;
+
+import org.hibernate.HibernateException;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * TODO: Descripcion de la clase. Los comentarios van en castellano.
+ * 
+ * 
+ * @author Marcelo Turrin
+ * @since Sep 13, 2010
+ */
+public class URIUserTypeTest {
+    private  URIUserType type;
+    private URI value;
+    
+    @Before
+    public void onBefore() throws URISyntaxException{
+        type = new URIUserType();
+        value = new URI("http://www.zaubersoftware.com");
+    }
+
+    /**
+     * Test method for {@link URIUserType#isMutable()}.
+     */
+    @Test
+    public void testIsMutable() {
+        Assert.assertFalse(type.isMutable());
+    }
+
+    /**
+     * Test method for {@link URIUserType#returnedClass()}.
+     */
+    @Test
+    public void testReturnedClass() {
+        Assert.assertEquals(URI.class, type.returnedClass());
+    }
+
+    /**
+     * Test method for {@link URIUserType#sqlTypes()}.
+     */
+    @Test
+    public void testSqlTypes() {
+        Assert.assertEquals(1, type.sqlTypes().length);
+    }
+
+    /**
+     * Test method for {@link URIUserType#nullSafeGet(ResultSet, String[], Object)}.
+     * @throws SQLException 
+     * @throws HibernateException 
+     * @throws URISyntaxException 
+     */
+    @Test
+    public void testNullSafeGet() throws HibernateException, SQLException, URISyntaxException {
+        final String rta = "respuesta";
+        ResultSet rs = Mockito.mock(ResultSet.class);
+        String[] names = { "no importa"};
+        Object owner = null;
+        
+        Assert.assertNull(type.nullSafeGet(rs, names, owner));
+        
+        Mockito.when(rs.getString(Mockito.any(String.class))).thenReturn(rta );
+        Assert.assertEquals(new URI(rta), type.nullSafeGet(rs, names, owner));
+    }
+    
+    /** nullSafeGet no puede recibir null en la lista de nombres*/
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullSafeGetNullNames() throws HibernateException, SQLException{
+        type.nullSafeGet(Mockito.mock(ResultSet.class), null, null);
+    }
+    
+    /** nullSafeGet no puede recibir null en el result set*/
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullSafeGetNullRs() throws HibernateException, SQLException{
+        type.nullSafeGet(null, new String[] {}, null);
+    }
+    
+    /** nullSafeGet no puede recibir fue la lista vacia */
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullSafeGetNoNames() throws HibernateException, SQLException{
+        type.nullSafeGet(Mockito.mock(ResultSet.class), new String[] {}, null);
+    }
+
+    /**
+     * Test method for {@link URIUserType#nullSafeSet(PreparedStatement, Object, int)}.  
+     */
+    @Test
+    public void testNullSafeSet() throws HibernateException, SQLException  {
+        final PreparedStatement st = Mockito.mock(PreparedStatement.class);
+        int index = 1;
+        
+        type.nullSafeSet(st, null, index );
+        Mockito.verify(st).setNull(Mockito.anyInt(), Mockito.anyInt());
+        Mockito.verify(st, Mockito.never()).setString(Mockito.anyInt(), Mockito.anyString());
+        
+        Mockito.reset(st);
+        type.nullSafeSet(st, value, index );
+        Mockito.verify(st, Mockito.never()).setNull(Mockito.anyInt(), Mockito.anyInt());
+        Mockito.verify(st).setString(Mockito.anyInt(), Mockito.anyString());
+        
+        
+    }
+
+    /**
+     * Test method for {@link URIUserType#assemble(java.io.Serializable, Object)}.
+     */
+    @Test
+    public void testAssemble() {
+        Assert.assertEquals(value, type.assemble(value,null));   
+    }
+
+    /**
+     * Test method for {@link URIUserType#deepCopy(Object)}. 
+     */
+    @Test
+    public void testDeepCopy() {
+        Assert.assertEquals(value, type.deepCopy(value));   
+    }
+
+    /**
+     * Test method for {@link URIUserType#disassemble(Object)}.
+     */
+    @Test
+    public void testDisassemble() {        
+        Assert.assertEquals(value, type.disassemble(value));   
+    }
+
+    /**
+     * Test method for {@link URIUserType#replace(Object, Object, Object)}.
+     */
+    @Test
+    public void testReplace() {
+        Assert.assertEquals(value, type.replace(value, null, null));   
+    }
+
+}
